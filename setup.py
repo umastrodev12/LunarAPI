3bc6810652f96d9275f59952e510c7a82cfe9d11
Astro <astrochanneloficial@gmail.com>
2026-03-16 19:51:10 -0300

    lá vem o setup!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lunarapi",
+    version="0.1.0",
+    description="An Simple Unnoficial Lunar Client API!",
+    author="Astro",
+    packages=find_packages(),
+    python_requires=">3.8",
+    url="https://github.com/umastrodev12/LunarAPI"
+
+)
